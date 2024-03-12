5495b55dfe4fae34d8116aec9d01ce6ee456846a
Xiantas <mat.moulat@gmail.com>
2024-03-12 17:36:03 +0100

    Basic image writing

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,16 @@ mod geometry;
 
 use image::RgbImage;
 
-fn main() {
+fn main() -> Result<(),()> {
     let mut img = RgbImage::new(1000, 1000);
+
+    for x in 100..510 {
+        for y in x..510 {
+            img.put_pixel(x, y, image::Rgb([(x/2) as u8, (y/2) as u8, ((x+y)/4) as u8]));
+        }
+    }
+
+    img.save("target/img.png").map_err(|_| ())?;
+
+    Ok(())
 }
