28149102a0d384f6c5bc9dab23b3614996e115a9
Xiantas <mat.moulat@gmail.com>
2024-03-12 16:21:38 +0100

    Tweaks

--- src/geometry/basics.rs
+++ src/geometry/basics.rs
@@ -8,16 +8,4 @@ impl Point {
     pub fn new(x: f32, y: f32) -> Point {
         Point{x, y}
     }
-
-    pub fn translate(&mut self, p2: Point) {
-        self.x += p2.x;
-        self.y += p2.y;
-    }
-}
-
-fn truc() {
-    let mut x = Point::new(0.0, 0.1);
-    let y = Point {x: 0.2, y: 0.3};
-
-    x.translate(y);
 }
