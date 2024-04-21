f092a75a8b6d61b37103d4c5754b8863e71ec21c
“Mmeche” <mattherb@live.fr>
2024-04-21 15:28:54 +0200

    first shaderBezier

---
+++ shaders/bezier.fs.glsl
@@ -0,0 +1,13 @@
+#version 330
+
+flat in int vIntensity;
+
+float colorIntensity(){
+    return exp(-10*pow(vIntensity,4));
+}
+
+out vec4 fFragColor;
+
+void main(){
+    fFragColor = vec4(vec3(1.,0.,0.)*colorIntensity(),1.);
+}
